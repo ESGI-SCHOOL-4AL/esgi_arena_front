107c01a6241621bd7b9605858327ce5c4ddcc9ca
SystemGlitch <jeremy.la@outlook.fr>
2020-04-17 18:07:10 +0200

    Add PathGrid

---
+++ components/PathGrid.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="path-grid">
+    <canvas ref="canvas" :width="width" :height="height"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PathGrid",
+  props: {
+    terrain: { type: Array, required: true },
+    path: { type: Array, required: true },
+    width: { type: Number, default: 400 },
+    height: { type: Number, default: 400 }
+  },
+  data() {
+    return {
+      canvas: null
+    }
+  },
+  computed: {
+    squareWidth() {
+      return this.width / this.terrain.length
+    },
+    squareHeight() {
+      return this.height / this.terrain.length
+    }
+  },
+  watch: {
+    terrain() {
+      this.draw()
+    },
+    path() {
+      this.draw()
+    }
+  },
+  mounted() {
+    this.canvas = this.$refs.canvas.getContext("2d")
+    this.draw()
+  },
+  methods: {
+    draw() {
+      this.clearCanvas()
+      this.drawTerrain()
+      this.drawPath()
+    },
+    clearCanvas() {
+      this.canvas.clearRect(0, 0, this.width, this.height)
+    },
+    drawTerrain() {
+      this.canvas.strokeStyle = "#000000"
+      this.canvas.fillStyle = "#000000"
+      this.canvas.lineWidth = 1
+      for (let i = 0; i < this.terrain.length; i++) {
+        for (let j = 0; j < this.terrain[i].length; j++) {
+          if (this.terrain[i][j]) {
+            this.canvas.fillRect(
+              j * this.squareWidth,
+              i * this.squareHeight,
+              this.squareWidth,
+              this.squareHeight
+            )
+          } else {
+            this.canvas.strokeRect(
+              j * this.squareWidth,
+              i * this.squareHeight,
+              this.squareWidth,
+              this.squareHeight
+            )
+          }
+        }
+      }
+    },
+    drawPath() {
+      this.canvas.strokeStyle = "#FF0000"
+      this.canvas.fillStyle = "#FF0000"
+      this.canvas.lineWidth = 10
+
+      if (this.path.length === 1) {
+        const x = this.path[0].x * this.squareWidth + this.squareWidth / 2
+        const y = this.path[0].y * this.squareHeight + this.squareHeight / 2
+        this.canvas.moveTo(x - 5, y)
+        this.canvas.lineTo(x + 5, y)
+      } else {
+        this.canvas.beginPath()
+
+        for (let i = 0; i < this.path.length; i++) {
+          const x = this.path[i].x * this.squareWidth + this.squareWidth / 2
+          const y = this.path[i].y * this.squareHeight + this.squareHeight / 2
+
+          if (i === 0) {
+            this.canvas.moveTo(x, y)
+          } else {
+            this.canvas.lineTo(x, y)
+          }
+        }
+      }
+      this.canvas.stroke()
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <section class="hero is-fullheight">
     <div class="hero-body">
       <div class="container has-text-centered">
+        <path-grid :terrain="terrain" :path="path" />
         <move-controls
           @movePressed="onMovePressed"
           @moveReleased="onMoveReleased"
@@ -13,14 +14,43 @@
 
 <script>
 import MoveControls from "~/components/MoveControls.vue"
+import PathGrid from "~/components/PathGrid.vue"
 
 export default {
   components: {
-    MoveControls
+    MoveControls,
+    PathGrid
+  },
+  data() {
+    return {
+      terrain: [
+        [0, 0, 1],
+        [0, 0, 1],
+        [0, 0, 1]
+      ],
+      path: [{ x: 0, y: 0 }],
+      currentPos: { x: 0, y: 0 }
+    }
   },
   methods: {
     onMovePressed(direction) {
       console.log(direction, "pressed")
+      switch (direction) {
+        case "up":
+          this.currentPos.y--
+          break
+        case "down":
+          this.currentPos.y++
+          break
+        case "left":
+          this.currentPos.x--
+          break
+        case "right":
+          this.currentPos.x++
+          break
+      }
+      const newPos = Object.assign({}, this.currentPos)
+      this.path.push(newPos)
     },
     onMoveReleased(direction) {
       console.log(direction, "released")
